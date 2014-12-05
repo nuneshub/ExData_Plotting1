b47fcc4e82f7537b71349b98ebcd5698d9f51bc2
Manuel Nunes <mcm.nunes@gmail.com>
2014-12-05 18:40:17 +0000

    course project 1

---
+++ plot1.R
@@ -0,0 +1,43 @@
+################################################################################
+# Exploratory Data Analysis
+# Course Project 1
+################################################################################
+# Preparing data set for all plots ---------------------------------------------
+# download data from source and unzip to R working directory
+
+Url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <- file.path(getwd(), "edaproject1.zip")
+download.file(Url, destfile=file, mode="wb")
+unzip(file)
+
+# reading data into R
+
+file <- file.path(getwd(), "household_power_consumption.txt")
+c <- "character"; n <- "numeric"
+colclasses <- c(c,c,n,n,n,n,n,n,n)
+dt <- read.table(file=file,header=TRUE,sep=";",quote="",na.strings="?",
+                 colClasses=colclasses,nrows=2075259,stringsAsFactors=FALSE)
+names(dt) <- tolower(names(dt))
+
+# subset for days: 1/2/2007 and 2/2/2007
+
+dt <- subset(dt, dt$date=="1/2/2007" | dt$date=="2/2/2007")
+
+# creates column "datetime" and formats it to as.POSIXct
+
+dt$datetime <- paste(dt$date,dt$time,sep=" ")
+dt$datetime <- as.POSIXct(strptime(dt$datetime, format="%d/%m/%Y %H:%M:%S"))
+
+# Plotting data - Plot 1 -------------------------------------------------------
+# backgound transparent as in https://github.com/rdpeng/ExData_Plotting1
+
+png(file="plot1.png",width=480,height=480,bg="transparent")
+
+hist(dt$global_active_power,main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",col="red")
+
+dev.off()
+
+
+
+
---
+++ plot3.R
@@ -0,0 +1,49 @@
+################################################################################
+# Exploratory Data Analysis
+# Course Project 1
+################################################################################
+# Preparing data set for all plots ---------------------------------------------
+# download data from source and unzip to R working directory
+
+Url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <- file.path(getwd(), "edaproject1.zip")
+download.file(Url, destfile=file, mode="wb")
+unzip(file)
+
+# reading data into R
+
+file <- file.path(getwd(), "household_power_consumption.txt")
+c <- "character"; n <- "numeric"
+colclasses <- c(c,c,n,n,n,n,n,n,n)
+dt <- read.table(file=file,header=TRUE,sep=";",quote="",na.strings="?",
+                 colClasses=colclasses,nrows=2075259,stringsAsFactors=FALSE)
+names(dt) <- tolower(names(dt))
+
+# subset for days: 1/2/2007 and 2/2/2007
+
+dt <- subset(dt, dt$date=="1/2/2007" | dt$date=="2/2/2007")
+
+# creates column "datetime" and formats it to as.POSIXct
+
+dt$datetime <- paste(dt$date,dt$time,sep=" ")
+dt$datetime <- as.POSIXct(strptime(dt$datetime, format="%d/%m/%Y %H:%M:%S"))
+
+# Plotting data - Plot 3 -------------------------------------------------------
+# backgound transparent as in https://github.com/rdpeng/ExData_Plotting1
+
+png(file="plot3.png",width=480,height=480,bg="transparent")
+
+plot(dt$datetime,dt$sub_metering_1,type="l",
+     xlab="",ylab="Energy sub metering")
+lines(dt$datetime,dt$sub_metering_2,col="red")
+lines(dt$datetime,dt$sub_metering_3,col="blue")
+
+legtxt <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+legcol <- c("black","red","blue")
+legend("topright",lty=1,col=legcol,legend=legtxt)
+
+dev.off()
+
+
+
+
